e7a1518c828aae28ecbd2885317e748b40444355
Thiago Ferradaes <tferradaes@gmail.com>
2025-09-18 12:09:13 -0300

    Cyrus
    Cyrus rocks added to his axe skill;

--- Assets/PROJECT/Scripts/Characters/Cyrus/Passive/CyrusPassiveManager.cs
+++ Assets/PROJECT/Scripts/Characters/Cyrus/Passive/CyrusPassiveManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,26 +9,22 @@ public class CyrusPassiveManager : PassiveSkillManager {
     #region Parameters
 
     public static CyrusPassiveManager Instance;
-    public enum WeaponType { Sword, Axe, Spear, Gun }
+
+    // Components
     CyrusPassiveSO _info;
-    HealthManager _healthManager;
-    PlayerSkillCooldownManager _playerSkillCooldownManager;
-    StatusManager _statusManager;
 
-    Action _onSpearChange;
-    Action _onGunChange;
-
-    // Nova passiva
+    // Atributes
+    float _currentAmountOfExp;
+    float _expMultiplier = 1f;
+    bool _rankUP;
+    CyrusRank _currentRank = CyrusRank.E;
     Dictionary<SkillSlot, int> _skillLevel = new() {
         { SkillSlot.SkillOne, 0 },
         { SkillSlot.SkillTwo, 0 },
         { SkillSlot.Ultimate, 0 },
     };
-    CyrusRank _currentRank = CyrusRank.E;
-    float _currentAmountOfExp;
-    float _expMultiplier = 1f;
-    bool _rankUP;
 
+    // Actions
     public event Action OnRankLevelUp, OnSkillLevelUp;
 
     #endregion
@@ -44,51 +41,18 @@ public class CyrusPassiveManager : PassiveSkillManager {
 
         Initialize(passive, parent);
 
-        _onSpearChange = () => ChangePassive(WeaponType.Spear);
-        _onGunChange = () => ChangePassive(WeaponType.Gun);
-
-        SpearAttackManager.OnWeaponChange -= _onSpearChange;
-        ShootUpUltimateManager.OnWeaponChange -= _onGunChange;
-
-        SpearAttackManager.OnWeaponChange += _onSpearChange;
-        ShootUpUltimateManager.OnWeaponChange += _onGunChange;
-
-        ChangePassive(WeaponType.Sword);
-
         gameObject.SetActive(true);
 
         UpgradeSkill();
 
         AditionalUIManager.Instance.InstantiateUI(_info.CyrusUI);
     }
-    private void OnDestroy() {
 
-        SpearAttackManager.OnWeaponChange -= _onSpearChange;
-        ShootUpUltimateManager.OnWeaponChange -= _onGunChange;
-    }
     void Initialize(PassiveSO passive, GameObject parent) {
         _info = passive as CyrusPassiveSO;
-        _healthManager = parent.GetComponent<HealthManager>();
-        _playerSkillCooldownManager = parent.GetComponent<PlayerSkillCooldownManager>();
-        _statusManager = parent.GetComponent<StatusManager>();
     }
 
     #endregion
-    void ChangePassive(WeaponType type) {
-        switch (type) {
-            case WeaponType.Sword:
-                break;
-            case WeaponType.Axe:
-                _healthManager.RecieveShield(_info.AmountOfFirstShieldRecieved, _info.ShieldDuration);
-                break;
-            case WeaponType.Spear:
-                _playerSkillCooldownManager.ResetCooldown(SkillSlot.Dash);
-                break;
-            case WeaponType.Gun:
-                _statusManager.ChangeStatus(StatusType.AttackSpeed, _info.AttackSpeedBuff, true, _info.AttackSpeedBuffDuration);
-                break;
-        }
-    }
 
     #endregion
 
@@ -118,7 +82,16 @@ public class CyrusPassiveManager : PassiveSkillManager {
     /// </summary>
     /// <param name="amountToMultiply"></param>
     /// <param name="increase"></param>
-    public void ChangeExpMultiplier(float amountToMultiply, bool increase) {
+    public void ChangeExpMultiplier(float amountToMultiply, bool increase, float duration) {
+        float realMultiplier = 1 + amountToMultiply / 100;
+        _expMultiplier = increase ? _expMultiplier * realMultiplier : _expMultiplier / realMultiplier;
+
+        StartCoroutine(ExpMultiplierTimer(amountToMultiply, !increase, duration));
+    }
+
+    IEnumerator ExpMultiplierTimer(float amountToMultiply, bool increase, float duration) {
+        yield return new WaitForSeconds(duration);
+
         float realMultiplier = 1 + amountToMultiply / 100;
         _expMultiplier = increase ? _expMultiplier * realMultiplier : _expMultiplier / realMultiplier;
     }
--- Assets/PROJECT/Scripts/Characters/Cyrus/Skills/Manager/AxeAttackManager.cs
+++ Assets/PROJECT/Scripts/Characters/Cyrus/Skills/Manager/AxeAttackManager.cs
@@ -13,6 +13,7 @@ public class AxeAttackManager : SkillObjectManager {
     // Atributes
     bool _isHoldingInput;
     float _chargeTimer;
+    int _skillLevel;
 
     // Coroutine
     Coroutine _chargeTimeCoroutine;
@@ -20,6 +21,8 @@ public class AxeAttackManager : SkillObjectManager {
     #endregion
 
     #region Methods
+
+    #region Override & Initialize
     public override void HandleInput(SkillSO skill, InputAction.CallbackContext ctx) {
 
         Initialize(skill);
@@ -43,6 +46,8 @@ public class AxeAttackManager : SkillObjectManager {
         if (_info == null) _info = skill as CyrusAxeSkillSO;
         if (_weaponManager == null) _weaponManager = parent.GetComponent<WeaponManager>();
 
+        _skillLevel = CyrusPassiveManager.Instance.ReturnSkillLevel(slot);
+
     }
 
     public override void OnPreCast(SkillSO skill) {
@@ -65,18 +70,21 @@ public class AxeAttackManager : SkillObjectManager {
         if (_info.PreCastOn && ConfigurationWhiteBoard.Instance.PreCastOn) SetSkillRangeIndicator(skill);
 
         // Checando nível
-        if (CyrusPassiveManager.Instance.ReturnSkillLevel(slot) >= 1) healthManager.RecieveShield(_info.AmountOfShield, _info.ShieldDuration);
+        if (_skillLevel >= 1) healthManager.RecieveShield(_info.AmountOfShield, _info.ShieldDuration);
     }
 
     public override void UseSkill(SkillSO skill) {
         animationCoroutine ??= StartCoroutine(Attack());
     }
 
+    #endregion
+
+    #region Courrotines
     IEnumerator ChargeTimer() {
 
         _chargeTimer = 0;
 
-        float maxChargeTime = CyrusPassiveManager.Instance.ReturnSkillLevel(slot) >= 2 ? _info.MaxChargeTime : _info.NewMaxChargeTime;
+        float maxChargeTime = _skillLevel >= 2 ? _info.MaxChargeTime : _info.NewMaxChargeTime;
 
         _weaponManager.OnEquipRightHand(_info.WeaponPrefab, _info.WeaponName, _info.WeaponPosition, _info.WeaponRotation);
 
@@ -122,45 +130,12 @@ public class AxeAttackManager : SkillObjectManager {
 
 
             if (prefabInfo.PrefabType == TypeOfSkillPrefab.Hitbox) {
-
-                GameObject preFab = PoolingManager.Instance.ReturnPrefabFromPool(prefabInfo.PreFabName,
-                    prefabInfo.PreFab, TypeOfSkillPrefab.Hitbox);
-                preFab.transform.SetParent(parent.transform, false);
-                preFab.transform.SetLocalPositionAndRotation(prefabInfo.PreFabPosition, Quaternion.identity);
-
-                float damage = ReturnDamage();
-                DamageContext newContext = new(
-                    damage,
-                    damage,
-                    prefabInfo.PrefabDuration,
-                    true,
-                    _info.DamageType,
-                    _info.EnemyTag,
-                    parent.GetComponent<StatusManager>(),
-                    new() {
-                        {ExtraDamageContextAtributes.BreakShield, (bool)ReturnBreakShield() }
-                    }
-                    );
-
-                InstantDamageHitBox hitbox = preFab.GetComponent<InstantDamageHitBox>();
-                hitbox.Initialize(newContext);
-
-                hitbox.OnHit += () => {
-                    energyManager.GainEnergy(_info.FlatEnergyGainPerHit);
-                    CyrusPassiveManager.Instance.GainExp(_info.AmountOfExpGain);
-                    if (CyrusPassiveManager.Instance.ReturnSkillLevel(slot) == 3) InstantiateBrokenRocks();
-                };
-
+                InstantiateHitBox(prefabInfo);
             }
             else {
-                GameObject preFab = PoolingManager.Instance.ReturnPrefabFromPool(prefabInfo.PreFabName,
-                    prefabInfo.PreFab, TypeOfSkillPrefab.VFX);
-                preFab.transform.SetParent(parent.transform, false);
-                preFab.transform.SetLocalPositionAndRotation(prefabInfo.PreFabPosition, Quaternion.identity);
-                preFab.GetComponent<VFXPreFab>().Initialize(prefabInfo.PrefabDuration);
+                InstantiateVFX(prefabInfo);
             }
         }
-
 
         while (anim.GetCurrentAnimatorStateInfo(0).fullPathHash == attackStateHash &&
                anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f) {
@@ -174,6 +149,9 @@ public class AxeAttackManager : SkillObjectManager {
         End();
     }
 
+    #endregion
+
+    #region Calculations
     float ReturnDamage() {
         float damage = (_chargeTimer * _info.MaxDamage) / _info.MaxChargeTime;
         return Mathf.Clamp(damage, _info.MinDamage, _info.MaxDamage);
@@ -183,7 +161,9 @@ public class AxeAttackManager : SkillObjectManager {
         if (_chargeTimer >= _info.MaxChargeTime) return true;
         else return false;
     }
+    #endregion
 
+    #region Stun
     public override void CancelSkill() {
         // parar corrotinas
         if (_chargeTimeCoroutine != null) {
@@ -200,10 +180,55 @@ public class AxeAttackManager : SkillObjectManager {
         End();
     }
 
+    #endregion
 
+    #region Instantiate
+    void InstantiateHitBox(SkillAnimationEvent prefabInfo) {
+        GameObject preFab = PoolingManager.Instance.ReturnPrefabFromPool(prefabInfo.PreFabName,
+                    prefabInfo.PreFab, TypeOfSkillPrefab.Hitbox);
+        preFab.transform.SetParent(parent.transform, false);
+        preFab.transform.SetLocalPositionAndRotation(prefabInfo.PreFabPosition, Quaternion.identity);
+
+        float damage = ReturnDamage();
+        DamageContext newContext = new(
+            damage,
+            damage,
+            prefabInfo.PrefabDuration,
+            true,
+            _info.DamageType,
+            _info.EnemyTag,
+            parent.GetComponent<StatusManager>(),
+            new() {
+                        {ExtraDamageContextAtributes.BreakShield, (bool)ReturnBreakShield() }
+            }
+            );
+
+        InstantDamageHitBox hitbox = preFab.GetComponent<InstantDamageHitBox>();
+        hitbox.Initialize(newContext);
+
+        // On Hit
+        hitbox.OnHit += () => {
+            energyManager.GainEnergy(_info.FlatEnergyGainPerHit);
+            CyrusPassiveManager.Instance.GainExp(_info.AmountOfExpGain);
+            if (_skillLevel == 3) InstantiateBrokenRocks();
+        };
+
+        // Cost
+        float duration = _info.expModifierDuration + (_skillLevel * _info.aditionalExpModifierDurationPerLevel);
+        CyrusPassiveManager.Instance.ChangeExpMultiplier(_info.ExpModifier, false, duration);
+    }
+
+    void InstantiateVFX(SkillAnimationEvent prefabInfo) {
+        GameObject preFab = PoolingManager.Instance.ReturnPrefabFromPool(prefabInfo.PreFabName,
+                    prefabInfo.PreFab, TypeOfSkillPrefab.VFX);
+        preFab.transform.SetParent(parent.transform, false);
+        preFab.transform.SetLocalPositionAndRotation(prefabInfo.PreFabPosition, Quaternion.identity);
+        preFab.GetComponent<VFXPreFab>().Initialize(prefabInfo.PrefabDuration);
+    }
     void InstantiateBrokenRocks() {
         GameObject preFab = PoolingManager.Instance.ReturnPrefabFromPool(_info.BrokenRocksName,
                     _info.BrokenRocksPrefab, TypeOfSkillPrefab.Hitbox);
+        preFab.transform.localScale = Vector3.one * _info.BrokenRockSize;
         preFab.transform.SetParent(parent.transform, false);
         preFab.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
 
@@ -223,6 +248,7 @@ public class AxeAttackManager : SkillObjectManager {
         ContinuosDamageHitBox hitbox = preFab.GetComponent<ContinuosDamageHitBox>();
         hitbox.Initialize(newContext);
     }
+    #endregion
 
     #endregion
 }
