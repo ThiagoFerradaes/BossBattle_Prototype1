0ae9e69d472e0a8bfb5f2efff02ca75c846697d0
Thiago Ferradaes <tferradaes@gmail.com>
2025-09-19 17:24:35 -0300

    Build
    Changes made to build the game

--- Assets/PROJECT/Scripts/Bosses/Kraken/KrakenRandomAttack.cs
+++ Assets/PROJECT/Scripts/Bosses/Kraken/KrakenRandomAttack.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 [CreateAssetMenu(menuName = "Kraken / RandomAttack")]
 public class KrakenRandomAttack : EnemyBehaviourSO {
