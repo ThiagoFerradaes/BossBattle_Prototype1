d2ec3e91e9323516edb602e475d82a7d6aaa3bda
Thiago Ferradaes <tferradaes@gmail.com>
2025-09-24 15:06:44 -0300

    Crab
    Camera transition in tide transition

---
+++ Assets/PROJECT/Scripts/Bosses/Crab/CrabArena/CrabCameraManager.cs
@@ -0,0 +1,40 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CrabCameraManager : MonoBehaviour
+{
+    [SerializeField] CinemachineCamera lowTideCamera;
+    [SerializeField] CinemachineCamera highTideCamera;
+
+    Action _onHandleHighTide, _onHandleLowTide;
+
+    #region Initialize
+    private void Awake() {
+        _onHandleHighTide = HandleHighTide;
+        _onHandleLowTide = HandleLowTide;
+
+        HandleLowTide();
+    }
+    private void Start() {
+        ArenaCrabManager.Instance.OnChangeToHighTide += _onHandleHighTide;
+        ArenaCrabManager.Instance.OnChangeToLowTide += _onHandleLowTide;
+    }
+    private void OnDestroy() {
+        ArenaCrabManager.Instance.OnChangeToHighTide -= _onHandleHighTide;
+        ArenaCrabManager.Instance.OnChangeToLowTide -= _onHandleLowTide;
+    }
+    #endregion
+
+    #region Handle Cameras
+    void HandleHighTide() {
+        highTideCamera.Priority = 1;
+        lowTideCamera.Priority = 0;
+    }
+
+    void HandleLowTide() {
+        highTideCamera.Priority = 0;
+        lowTideCamera.Priority = 1;
+    }
+    #endregion
+}
