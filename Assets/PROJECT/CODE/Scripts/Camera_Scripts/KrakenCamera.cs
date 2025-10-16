b5ac0dcc4f93c2993204856128a14653fe035000
Samuel Azeredo <89816433+Eujota87@users.noreply.github.com>
2025-10-16 15:48:31 -0300

    Camera Shake on KrakenCamera.cs
    Added Camera Shake on KrakenCamera.cs
    
    the coroutine is started via ShakeCamera(), but we need to change it's implementation later to work properly with the OnDamageTaken event

--- Assets/PROJECT/CODE/Scripts/Camera_Scripts/KrakenCamera.cs
+++ Assets/PROJECT/CODE/Scripts/Camera_Scripts/KrakenCamera.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class KrakenCamera : MonoBehaviour {
@@ -6,6 +8,12 @@ public class KrakenCamera : MonoBehaviour {
     [Header("Atributes")]
     [SerializeField] float rotationSpeed = 2f;
     [SerializeField] float deadAngle = 3f;
+
+    [Header("Camera Shake")]
+    [SerializeField] bool debugStart = false;
+    [SerializeField, Range(1, 5)] float shakeIntensity = 1f;
+    [SerializeField] AnimationCurve shakeCurve;
+    [SerializeField, Range(0, 1)] float shakeDuration = 1f;
 
     // Components
     Transform _player;
@@ -16,8 +24,12 @@ public class KrakenCamera : MonoBehaviour {
 
     private void Start() {
         _player = PlayerManager.Instance.Player.transform;
+        _player.GetComponent<HealthManager>().OnDamageTaken += ShakeCamera;
+
     }
-    void Update() {
+
+    void Update()
+    {
         if (_player == null) return;
 
         Vector3 playerDir = transform.position - _player.position;
@@ -28,8 +40,38 @@ public class KrakenCamera : MonoBehaviour {
         Quaternion targetRotation = Quaternion.LookRotation(playerDir);
         float angle = Quaternion.Angle(transform.rotation, targetRotation);
 
-        if (angle > deadAngle) {
+        if (angle > deadAngle)
+        {
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        //debug shake
+        if (debugStart)
+        {
+            debugStart = false;
+            StartCoroutine(CameraShaking());
+        }
+
+
+    }
+
+    private void ShakeCamera(float obj)
+    {
+        StartCoroutine(CameraShaking());
+        throw new NotImplementedException();
+    }
+
+    IEnumerator CameraShaking()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < shakeDuration)
+        {
+            Vector3 startPosition = transform.position;
+            elapsedTime += Time.deltaTime;
+            float shakeStrength = shakeCurve.Evaluate(elapsedTime / shakeDuration) * shakeIntensity;
+            transform.position = startPosition + UnityEngine.Random.insideUnitSphere * shakeStrength;
+            yield return null;
+            transform.position = startPosition;
         }
     }
 
