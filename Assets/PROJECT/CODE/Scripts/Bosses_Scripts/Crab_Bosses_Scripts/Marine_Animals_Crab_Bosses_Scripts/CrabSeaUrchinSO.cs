c6e25cbf6a5342099899f48a852790b99b88ec73
Thiago Ferradaes <tferradaes@gmail.com>
2025-10-30 13:31:28 -0300

    Crab
    Crab jump fixed;

--- Assets/PROJECT/CODE/Scripts/Bosses_Scripts/Crab_Bosses_Scripts/Marine_Animals_Crab_Bosses_Scripts/CrabSeaUrchinSO.cs
+++ Assets/PROJECT/CODE/Scripts/Bosses_Scripts/Crab_Bosses_Scripts/Marine_Animals_Crab_Bosses_Scripts/CrabSeaUrchinSO.cs
@@ -9,7 +9,6 @@ public class CrabSeaUrchinSO : CrabMarineAnimalSO {
     [SerializeField] float explosionRadius;
  
     [Header("Damage atributes")]
-    [SerializeField] DamageAtributes damageAtributes;
     [SerializeField] List<SkillAnimationEvent> prefabs;
 
     CrabMarineAnimal _parent;
@@ -18,8 +17,6 @@ public class CrabSeaUrchinSO : CrabMarineAnimalSO {
         base.OnTrigger(other, parent);
 
         _parent = parent;
-
-        Collider[] colliders = Physics.OverlapSphere(parent.transform.position, explosionRadius);
 
         if (prefabs != null) {
             for (int i = 0; i < prefabs.Count; i++) {
@@ -35,7 +32,7 @@ public class CrabSeaUrchinSO : CrabMarineAnimalSO {
         hitbox.transform.localScale = explosionRadius * Vector3.one;
 
         DamageContext context = new(
-            damageAtributes,
+            Atributes,
             _parent.CrabManager.GetComponent<StatusManager>()
             );
 
