b49152abeb5d2d8b942d190d215ede76690661a0
Nicolas <niconunessoares@gmail.com>
2025-11-07 19:55:01 -0300

    set using

--- Assets/PROJECT/CODE/Scripts/Characters_Scripts/Lilian_Characters_Scripts/Skills_Lilian_Character_Scripts/Managers_Skills_Lilian_Character_Scripts/LilianBaseAttackManager.cs
+++ Assets/PROJECT/CODE/Scripts/Characters_Scripts/Lilian_Characters_Scripts/Skills_Lilian_Character_Scripts/Managers_Skills_Lilian_Character_Scripts/LilianBaseAttackManager.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class LilianBaseAttackManager : SkillObjectManager
@@ -78,7 +74,7 @@ public class LilianBaseAttackManager : SkillObjectManager
         // Cooldown
         cooldownManager.SetCooldownSingleCharge(slot, _info.Cooldown);
 
-        // Resetando a velocidade da animação
+        // Resetando a velocidade da animaï¿½ï¿½o
         anim.SetFloat(_info.AttackSpeedAnimationParameter, 1);
 
         // Attack Index
