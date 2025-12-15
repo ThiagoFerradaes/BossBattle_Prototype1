3e54647c014599c0a2d7dd70e11607733dde3714
Thiago Ferradaes <tferradaes@gmail.com>
2025-12-15 19:55:21 -0300

    Bastian
    Started alternative skills

--- Assets/PROJECT/CODE/Scripts/Characters_Scripts/Bastian_Characters_Scripts/Skills_Bastian_Characters_Scripts/Managers_Skills_Bastian_Characters_Scripts/BastianSteamPunchManager.cs
+++ Assets/PROJECT/CODE/Scripts/Characters_Scripts/Bastian_Characters_Scripts/Skills_Bastian_Characters_Scripts/Managers_Skills_Bastian_Characters_Scripts/BastianSteamPunchManager.cs
@@ -1,68 +1,48 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class BastianIgnisManager : SkillObjectManager {
-    // Components
-    BastianIgnisSO _info;
-
+public class BastianSteamPunchManager : SkillObjectManager
+{
+    BastianSteamPunchSO _info;
     float _attackSpeedMultiplier;
-    public override void HandleInput(SkillSO skill, InputAction.CallbackContext ctx) {
-        if (!BastianPassiveManager.Instance.CanShoot) {
-            return;
-        }
-
-        if (ctx.phase == InputActionPhase.Started) {
-            _preCasted = true;
-            PreCast(skill);
-        }
-        if (ctx.phase == InputActionPhase.Canceled && _preCasted) {
-            ReleaseInput(skill);
-        }
-    }
-
     public override void UseSkill(SkillSO skill) {
-
-
-        if (_info == null) _info = skill as BastianIgnisSO;
-
-        if (!gameObject.activeInHierarchy) {
-            gameObject.SetActive(true);
-        }
+        Initialize(skill);
 
         animationCoroutine ??= StartCoroutine(AttackCoroutine(0, _info.AnimationParameter, _info.AnimationName, 0));
     }
 
+    private void Initialize(SkillSO skill) {
+        if (_info == null) _info = skill as BastianSteamPunchSO;
+
+        if (!gameObject.activeInHierarchy) gameObject.SetActive(true);
+    }
+
     public override void FirstFunc() {
-        cooldownManager.SetCooldownWithCharges(slot, _info);
+        cooldownManager.SetCooldownSingleCharge(slot, _info.Cooldown);
         _attackSpeedMultiplier = GetAttackSpeedMultiplier();
 
         skillManager.SkillIsInAnimation(true);
 
         anim.SetFloat(_info.AttackSpeedAnimationParameter, _attackSpeedMultiplier);
     }
-
-    public override void FourthFunc() {
-        // Resetando a velocidade da animação
-        anim.SetFloat(_info.AttackSpeedAnimationParameter, 1);
-
-        // Corrotina
-        animationCoroutine = null;
-
-        skillManager.SkillIsInAnimation(false);
-
-        EndWithUnblockSkills();
-    }
     float GetAttackSpeedMultiplier() {
         float baseSpeed = statusManager.ReturnStatusValue(StatusType.AttackSpeed);
         return Mathf.Max(0.1f, baseSpeed);
     }
 
-    public override void InstantiateHitBox(SkillAnimationEvent prefabInfo) {
-        GameObject preFab = PoolingManager.Instance.ReturnPrefabFromPool(prefabInfo.PreFab, TypeOfSkillPrefab.Hitbox);
+    public override void FourthFunc() {
+        // Resetando a velocidade da animação
+        anim.SetFloat(_info.AttackSpeedAnimationParameter, 1);
+
+        base.FourthFunc();
+
+        EndWithUnblockSkills();
+    }
+
+    public override void InstantiateHitBox(SkillAnimationEvent prefab) {
+        GameObject preFab = PoolingManager.Instance.ReturnPrefabFromPool(prefab.PreFab, TypeOfSkillPrefab.Hitbox);
 
         preFab.transform.localScale = _info.SkillDamageAtributes.Size;
-        preFab.transform.SetPositionAndRotation(parent.transform.position + prefabInfo.PreFabPosition, parent.transform.rotation);
+        preFab.transform.SetPositionAndRotation(parent.transform.position + prefab.PreFabPosition, parent.transform.rotation);
 
         float pen = BastianPassiveManager.Instance.ReturnMinHeat(HeatArea.SuperHeatArea) ? _info.PenetrationOnSuperHeat : 0;
         float critChance = BastianPassiveManager.Instance.ReturnMinHeat(HeatArea.OverHeatArea) ? _info.CritChanceOverHeat : 0;
@@ -73,7 +53,7 @@ public class BastianIgnisManager : SkillObjectManager {
         atributes.ExtraAtributes[ExtraDamageContextAtributes.Penetration] = pen;
         atributes.ExtraAtributes[ExtraDamageContextAtributes.CritRate] = critChance;
         atributes.ExtraAtributes[ExtraDamageContextAtributes.CritDamage] = critDamage;
-        
+
         DamageContext newContext = new(
             atributes,
             parent.GetComponent<StatusManager>()
@@ -85,9 +65,5 @@ public class BastianIgnisManager : SkillObjectManager {
         hitbox.OnHit += () => {
             energyManager.GainEnergy(_info.FlatEnergyGainPerHit);
         };
-
-        if (BastianPassiveManager.Instance.ReturnMaxHeat(HeatArea.SuperHeatArea))
-            BastianPassiveManager.Instance.GainHeat(_info.HeatGain);
-        else BastianPassiveManager.Instance.GainHeat(1);
     }
 }
