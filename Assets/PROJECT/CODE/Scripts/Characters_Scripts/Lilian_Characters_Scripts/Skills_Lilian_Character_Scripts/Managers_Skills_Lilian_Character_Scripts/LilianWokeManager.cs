0efa12f5e0a5863939968742965577f2b36000e3
Thiago Ferradaes <tferradaes@gmail.com>
2026-01-07 19:00:00 -0300

    Lillian
    Lillian woke up skill added

--- Assets/PROJECT/CODE/Scripts/Characters_Scripts/Lilian_Characters_Scripts/Skills_Lilian_Character_Scripts/Managers_Skills_Lilian_Character_Scripts/LilianWokeManager.cs
+++ Assets/PROJECT/CODE/Scripts/Characters_Scripts/Lilian_Characters_Scripts/Skills_Lilian_Character_Scripts/Managers_Skills_Lilian_Character_Scripts/LilianWokeManager.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
 
-public class LilianWokeManager : MonoBehaviour
+public class LilianWokeManager : SkillObjectManager
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    LilianWokeSO _info;
+
+    public override void UseSkill(SkillSO skill) {
+        base.UseSkill(skill);
+
+        Initialize(skill);
+
+        animationCoroutine ??= StartCoroutine(AttackCoroutine(0, _info.AnimationParameter, _info.AnimationName, 0));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Initialize(SkillSO skill) {
+        if (_info == null) _info = skill as LilianWokeSO;
+
+        if (!gameObject.activeInHierarchy) gameObject.SetActive(true);
+    }
+
+    public override void FirstFunc() {
+        base.FirstFunc();
+
+        cooldownManager.SetCooldownSingleCharge(slot, _info.Cooldown);
+    }
+
+    public override void FourthFunc() {
+        base.FourthFunc();
+
+        EndWithUnblockSkills();
+    }
+
+    public override void InstantiateHitBox(SkillAnimationEvent prefabinfo) {
+        GameObject prefab = PoolingManager.Instance.ReturnPrefabFromPool(prefabinfo.PreFab, TypeOfSkillPrefab.Hitbox);
+
+        prefab.transform.localScale = _info.SkillDamageAtributes.Size;
+        prefab.transform.SetPositionAndRotation(parent.transform.position, parent.transform.rotation);
+
+        DamageContext newContext = new(
+            _info.SkillDamageAtributes,
+            parent.GetComponent<StatusManager>()
+            );
+
+        ContinuosDamageHitBox hitbox = prefab.GetComponent<ContinuosDamageHitBox>();
+        hitbox.Initialize(newContext);
     }
 }
--- Assets/PROJECT/CODE/Scripts/Characters_Scripts/Lilian_Characters_Scripts/Skills_Lilian_Character_Scripts/SO_Skills_Lilian_Character_Scripts/LilianWokeSO.cs
+++ Assets/PROJECT/CODE/Scripts/Characters_Scripts/Lilian_Characters_Scripts/Skills_Lilian_Character_Scripts/SO_Skills_Lilian_Character_Scripts/LilianWokeSO.cs
@@ -1,6 +1,10 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Characters/ Skills/ Lilian/ Woke")]
 public class LilianWokeSO : CommonSkillSO {
-    
+    [Header("Animation")]
+    [Foldout("Specific")] public string AnimationParameter;
+    [Foldout("Specific")] public string AnimationName;
+
 }
