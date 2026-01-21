8ed9a7a43a2879b89ce9617c28c450ca7f65bb20
Thiago Ferradaes <tferradaes@gmail.com>
2026-01-21 19:42:33 -0300

    Gracia
    Changes to Gracia's passiva;
    Gracia's skills scripts were created;

--- Assets/PROJECT/CODE/Scripts/Characters_Scripts/Gracia_Characters_Script/Passive_Gracia_Characters_Script/GraciaPassiveManager.cs
+++ Assets/PROJECT/CODE/Scripts/Characters_Scripts/Gracia_Characters_Script/Passive_Gracia_Characters_Script/GraciaPassiveManager.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 
 public enum GraciaTypeOfSkill { Left, Right };
+public enum GraciaAura { Blue, Yellow, Red, Green, Null };
 public class GraciaPassiveManager : PassiveSkillManager {
     #region Paramethers
 
@@ -10,6 +11,7 @@ public class GraciaPassiveManager : PassiveSkillManager {
 
     float _currentLeftBarValue, _currentRightBarValue;
     int _currentLeftBarArea, _currentRightBarArea;
+    GraciaAura _currentAura, _leftAura, _rightAura;
 
     public Action<float, GraciaTypeOfSkill> OnGraciaBarValueChanged;
     public Action<int, GraciaTypeOfSkill> OnGraciaBarAreaChanged;
@@ -45,7 +47,7 @@ public class GraciaPassiveManager : PassiveSkillManager {
     /// </summary>
     /// <param name="amountToChange"></param>
     /// <param name="type"></param>
-    public void ChangeBarValue(float amountToChange, GraciaTypeOfSkill type) {
+    public void ChangeBarValue(float amountToChange, GraciaTypeOfSkill type, GraciaAura aura) {
         switch (type) {
             case GraciaTypeOfSkill.Left:
                 // Alterando o valor da barra
@@ -55,6 +57,9 @@ public class GraciaPassiveManager : PassiveSkillManager {
                 if (_currentLeftBarValue >= _info.ValueToEnterArea3) _currentLeftBarArea = 3;
                 else if (_currentLeftBarValue >= _info.ValueToEnterArea2) _currentLeftBarArea = 2;
                 else _currentLeftBarArea = 1;
+
+                // Alterando a aura atual
+                if (_currentLeftBarValue > _currentRightBarValue) _currentAura = _leftAura;
 
                 // Avisando que o valor da barra alterou
                 OnGraciaBarValueChanged?.Invoke(_currentLeftBarValue, GraciaTypeOfSkill.Left);
@@ -69,12 +74,16 @@ public class GraciaPassiveManager : PassiveSkillManager {
                 else if (_currentRightBarValue >= _info.ValueToEnterArea2) _currentRightBarArea = 2;
                 else _currentRightBarArea = 1;
 
+                // Alterando a aura atual
+                if (_currentRightBarValue > _currentLeftBarValue) _currentAura = _rightAura;
+
                 // Avisando que o valor da barra alterou
                 OnGraciaBarValueChanged?.Invoke(_currentRightBarValue, GraciaTypeOfSkill.Right);
                 OnGraciaBarAreaChanged?.Invoke(_currentRightBarArea, GraciaTypeOfSkill.Right);
                 break;
             default: break;
         }
+       
     }
 
     #endregion
@@ -88,5 +97,22 @@ public class GraciaPassiveManager : PassiveSkillManager {
         }
         return 0;
     }
+
+    public GraciaAura ReturnCurrentAura() => _currentAura;
+    #endregion
+
+    #region Setters
+
+    public void SetAura(GraciaTypeOfSkill type, GraciaAura aura) {
+        switch (type) {
+            case GraciaTypeOfSkill.Left:
+                _leftAura = aura;
+                break;
+            case GraciaTypeOfSkill.Right:
+                _rightAura = aura;
+                break;
+        }
+    }
+
     #endregion
 }
