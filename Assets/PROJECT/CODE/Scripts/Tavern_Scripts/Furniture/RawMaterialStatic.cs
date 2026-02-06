bb83f46bf3a2315358d247094ef29cf97404c50c
Nicolas <135265220+N-S-N@users.noreply.github.com>
2026-02-06 17:35:16 -0300

    no obsoleto System

--- Assets/PROJECT/CODE/Scripts/Tavern_Scripts/Furniture/RawMaterialStatic.cs
+++ Assets/PROJECT/CODE/Scripts/Tavern_Scripts/Furniture/RawMaterialStatic.cs
@@ -173,7 +173,7 @@ public class RawMaterialStatic : MonoBehaviour
             
             Debug.Log("Inventory save data saved successfully!");
         }
-        catch (Exception e)
+        catch (Exception)
         {
             //Debug.LogError($"Error saving inventory configuration: {e.Message}");
             //Debug.LogError($"Stack trace: {e.StackTrace}");
@@ -202,7 +202,7 @@ public class RawMaterialStatic : MonoBehaviour
             FurnitureQuantity.AddRange(loadedData.saveFurniture.ToDictionary());
             timeGame = loadedData.timeGame;
         }
-        catch (Exception e)
+        catch (Exception)
         {
             //Debug.LogError($"Error loading inventory configuration: {e.Message}");
             //Debug.LogError($"Stack trace: {e.StackTrace}");
