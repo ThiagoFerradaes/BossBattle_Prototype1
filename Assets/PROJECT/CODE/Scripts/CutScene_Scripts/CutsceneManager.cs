2f737540b9b388e0e2416500f3891b0c266fb96a
Nicolas <135265220+N-S-N@users.noreply.github.com>
2026-02-20 15:37:52 -0300

    CutScene
    Set cutScene Script

--- Assets/PROJECT/CODE/Scripts/CutScene_Scripts/CutsceneManager.cs
+++ Assets/PROJECT/CODE/Scripts/CutScene_Scripts/CutsceneManager.cs
@@ -6,58 +6,135 @@ using UnityEngine.Video;
 
 public class CutsceneManager : MonoBehaviour
 {
-    public double time;
-
     public static event Action OnCutsceneStart;
     
     public static event Action OnCutsceneEnd;
 
     private void EndCutscene()
     {
-        eventCutscene.Invoke();
+        eventCutsceneEnd.Invoke();
         OnCutsceneEnd?.Invoke();
+        if (playerCanMovement == PlayerMovement.Yes) return;
+        
+        if(!PlayerManager.Instance.Player.TryGetComponent(out PlayerMovementManager movement))return;
+        movement.BlockMovement(false);
     }
 
+    [Header("Events")]
     public UnityEvent eventCutscene;
+    public UnityEvent eventCutsceneEnd;
     
+    [Space(10)]
+    [Header("Types")]
     public TypeCutscene typeCutscene;
+    public TypeStartCutscene typeStartCutscene;
+    public PlayerMovement playerCanMovement;
+    
+    [Space(10)]
+    [Header("Cutscene")]
+    public VideoPlayer videoPlayer;
+    public PlayableDirector timelineClip;
+
     
     public enum TypeCutscene
     {
         PlayableDirector,
         VideoPlayer,
     }
+
+    public enum TypeStartCutscene
+    {
+        Start,
+        Enable,
+        Awake,
+        Function,
+        Events,
+        TriggerEnter,
+        TriggerExit,
+        CollisionEnter,
+        CollisionExit,
+    }
+
+    public enum PlayerMovement
+    {
+        No,
+        Yes,
+    }
     
-    public VideoPlayer videoPlayer;
-    
-    public PlayableDirector timelineClip;
+    private void OnEnable()
+    {
+        if (typeStartCutscene == TypeStartCutscene.Events)
+        {
+            eventCutscene.RemoveListener(StartCutsceneOn);
+            eventCutscene.AddListener(StartCutsceneOn);
+            return;
+        }
+        
+        if(typeStartCutscene != TypeStartCutscene.Enable) return;
+        StartCutsceneOn();
+    }
     
     private void Start()
     {
-        time = StartCutsceneOn();
+        if(typeStartCutscene != TypeStartCutscene.Start) return;
+        StartCutsceneOn();
     }
     
-    public double StartCutsceneOn()
+    private void Awake()
+    {
+        if (typeStartCutscene != TypeStartCutscene.Awake)
+        {
+            return;
+        }
+        StartCutsceneOn();
+    }
+
+    private void OnCollisionEnter()
+    {
+        if (typeStartCutscene != TypeStartCutscene.CollisionEnter) return;
+        StartCutsceneOn();
+    }
+
+    private void OnCollisionExit()
+    {
+        if (typeStartCutscene != TypeStartCutscene.CollisionExit) return;
+        StartCutsceneOn();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (typeStartCutscene != TypeStartCutscene.TriggerEnter) return;
+        StartCutsceneOn();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (typeStartCutscene != TypeStartCutscene.TriggerExit) return;
+        StartCutsceneOn();
+    }
+
+    public void StartCutsceneOn()
     {
         OnCutsceneStart?.Invoke();
-
+        
         switch (typeCutscene)
         {
             case TypeCutscene.PlayableDirector:
-                timelineClip.stopped += director => EndCutscene();
+                timelineClip.stopped += _ => EndCutscene();
                 timelineClip.time = 0;
                 timelineClip.Play();
-                return timelineClip.duration;
                 break;
             case TypeCutscene.VideoPlayer:
-                videoPlayer.seekCompleted += player => EndCutscene();
+                videoPlayer.seekCompleted += _ => EndCutscene();
                 videoPlayer.Play();
-                return videoPlayer.time;
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
         }
 
-        return 0;
+        if (playerCanMovement == PlayerMovement.Yes) return;
+
+        if(!PlayerManager.Instance.Player.TryGetComponent(out PlayerMovementManager movement))return;
+        movement.BlockMovement(true);
     }
 }
