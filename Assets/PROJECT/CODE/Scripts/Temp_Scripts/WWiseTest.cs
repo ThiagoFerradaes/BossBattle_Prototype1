d47415cf3c2d6305f80d719f60f201698abaa3b4
Thiago Ferradaes <tferradaes@gmail.com>
2026-03-02 15:55:16 -0300

    WWise tests

---
+++ Assets/PROJECT/CODE/Scripts/Temp_Scripts/WWiseTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WWiseTest : MonoBehaviour
+{
+    [SerializeField] private AK.Wwise.Event myEvent = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
