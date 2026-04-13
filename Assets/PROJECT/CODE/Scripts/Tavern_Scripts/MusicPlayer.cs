5940435a7136b2f3c2e3a1b927f34e4e409c5431
Thiago Ferradaes <tferradaes@gmail.com>
2026-04-13 18:14:10 -0300

    Music
    Kraken music added to the game;
    
    Some values were adjusted in Bastian's base attack animation speed;

--- Assets/PROJECT/CODE/Scripts/Tavern_Scripts/MusicPlayer.cs
+++ Assets/PROJECT/CODE/Scripts/Tavern_Scripts/MusicPlayer.cs
@@ -1,10 +1,18 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] bool hasSwitch;
+    [SerializeField, ShowIf("hasSwitch")] AK.Wwise.Switch musicSwitch;
+
     [SerializeField] AK.Wwise.Event newMusic = null;
 
     private void Start() {
         AkUnitySoundEngine.StopAll();
+
+        if (hasSwitch) musicSwitch.SetValue(gameObject);
+
         newMusic.Post(gameObject);
     }
 
