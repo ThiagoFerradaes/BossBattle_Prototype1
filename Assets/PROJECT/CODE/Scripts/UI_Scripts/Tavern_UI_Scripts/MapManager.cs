a12212d9e1a769d8932e2ed9d3bad33c6dcad657
Thiago Ferradaes <tferradaes@gmail.com>
2026-04-16 19:35:30 -0300

    Map
    A script was created to handle the responsibilities of the level difficulty screen;
    
    Functions related to the level difficulty screen were extracted into the new script;

--- Assets/PROJECT/CODE/Scripts/UI_Scripts/Tavern_UI_Scripts/MapManager.cs
+++ Assets/PROJECT/CODE/Scripts/UI_Scripts/Tavern_UI_Scripts/MapManager.cs
@@ -31,34 +31,12 @@ public class MapManager : MonoBehaviour {
     [Foldout("First Map"), SerializeField] Button TestIslandButton;
     [Foldout("First Map"), SerializeField] BossDescription TestIslandDescription;
     [Foldout("First Map"), SerializeField] GameObject selectedIslandIcon;
-    [Foldout("First Map"), SerializeField] Sprite normalSailButtonSprite;
-    [Foldout("First Map"), SerializeField] Sprite enterSailButtonSprite;
     [Foldout("First Map"), SerializeField] Sprite enterExitButtonSprite;
     [Foldout("First Map"), SerializeField] Sprite exitExitButtonSprite;
 
-    [Foldout("Second Map"), SerializeField] GameObject SecondMap;
-    [Foldout("Second Map"), SerializeField] Image BossImage;
-    [Foldout("Second Map"), SerializeField] Image SelectedCharacterIcon;
-    [Foldout("Second Map"), SerializeField] LocalizeSpriteEvent IsleName;
-    [Foldout("Second Map"), SerializeField] TextMeshProUGUI BossDescription;
-    [Foldout("Second Map"), SerializeField] Button CloseSecondMapButton;
-    [Foldout("Second Map"), SerializeField] Button SailButton;
-    [Foldout("Second Map"), SerializeField] Button ChangeCharacterButton;
-    [Foldout("Second Map"), SerializeField] List<Sprite> listOfDificultySpritesActive;
-    [Foldout("Second Map"), SerializeField] List<Sprite> listOfDificultySpritesDesactive;
-    [Foldout("Second Map"), SerializeField] List<Image> ListOfDifficultyImages;
-    [Foldout("Second Map"), SerializeField] List<Button> ListOfDifficultyButtons;
-    [Foldout("Second Map"), SerializeField] List<Image> ListOfDifficultyLocks;
-    [Foldout("Second Map"), SerializeField] Color difficultySelectedColor;
-    [Foldout("Second Map"), SerializeField] Color difficultyDeselectedColor;
-
-    int _currentDifficulty = 0;
-
-    CharacterSelectionManager _characterSelectionManager;
+    [SerializeField] BossDifficultyManager bossDifficultyManager;
 
     public event Action OnCloseMap;
-
-    Action<CharacterSO> _onChangeSelectedCharacter;
 
     PlayerInputHandlerManager _handler;
 
@@ -67,17 +45,9 @@ public class MapManager : MonoBehaviour {
     private void Awake() {
         TurnMapOff();
 
-        _characterSelectionManager = GetComponent<CharacterSelectionManager>();
-        _onChangeSelectedCharacter = OnChangeSelectedCharacter;
-
         SetButtons();
     }
-    private void Start() {
-        CurrentSelectedCharacterWhiteBoard.Instance.OnSelectedCharacterChanged += _onChangeSelectedCharacter;
-    }
-    private void OnDestroy() {
-        CurrentSelectedCharacterWhiteBoard.Instance.OnSelectedCharacterChanged -= _onChangeSelectedCharacter;
-    }
+
     public void InitializeMap(PlayerInputHandlerManager handler = null) {
         _handler = handler;
 
@@ -93,17 +63,12 @@ public class MapManager : MonoBehaviour {
 
             Button button = DictionaryOfButtons[phase].GetComponent<Button>();
             button.interactable = board.ReturnListOfUnlockedPhasesByBoss().ContainsKey(phase);
-
-
         }
-
-        OnChangeSelectedCharacter(CurrentSelectedCharacterWhiteBoard.Instance.ReturnSelectedCharacterSO());
 
         mapScreen.SetActive(true);
     }
     #endregion
 
-    void OnChangeSelectedCharacter(CharacterSO newCharacter) { SelectedCharacterIcon.sprite = newCharacter.CharacterIcon; }
     void SetButtons() {
         foreach (var pair in DictionaryOfButtons) {  // ILHAS
             if (!DictinaryOfDescritions.TryGetValue(pair.Key, out var description)) continue;
@@ -114,27 +79,18 @@ public class MapManager : MonoBehaviour {
             button.onClick.AddListener(() => InslandButtonFunc(button, localDescription));
         }
 
-        for (int i = 0; i < ListOfDifficultyButtons.Count; i++) {
-            int index = i;
-            ListOfDifficultyButtons[i].onClick.AddListener(() => SelectDifficulty(index));
-        }
-
         CloseMapButton.onClick.AddListener(() => {
             CloseMapScreen();
         });
 
-        CloseSecondMapButton.onClick.AddListener(() => {
-            CloseBossDifficultyScreen();
-        });
 
-        TestIslandButton.onClick.AddListener(() => TurnBossDiffucltyScreenOn(TestIslandDescription));
 
-        ChangeCharacterButton.onClick.AddListener(() => _characterSelectionManager.Initialize());
+        TestIslandButton.onClick.AddListener(() => bossDifficultyManager.TurnBossDifficultyScreenOn(TestIslandDescription));   
     }
 
     void InslandButtonFunc(Button button, BossDescription description) {
         if (button != _selectedIslandButton) {
-            TurnBossDiffucltyScreenOn(description);
+            bossDifficultyManager.TurnBossDifficultyScreenOn(description);
             TurnIslandSelectSpriteOn(description.Boss);
         }
         else {
@@ -143,9 +99,8 @@ public class MapManager : MonoBehaviour {
     }
     void CloseMapScreen() {
         TurnMapOff();
+        TurnDifficultyScreenOf();
         OnCloseMap?.Invoke();
-        SecondMap.SetActive(false);
-        SailButton.gameObject.SetActive(false);
     }
     void CloseBossDifficultyScreen() {
         TurnDifficultyScreenOf();
@@ -153,12 +108,11 @@ public class MapManager : MonoBehaviour {
     }
     void TurnMapOff() {
         if (_handler != null) _handler.SetCanInput(true);
+        TurnAllIslandSelectSpriteOff();
         mapScreen.SetActive(false);
     }
     void TurnDifficultyScreenOf() {
-
-        SecondMap.SetActive(false);
-        SailButton.gameObject.SetActive(false);
+        bossDifficultyManager.TurnBossDifficultyScreenOff();
         CloseMapButton.gameObject.SetActive(true);
     }
 
@@ -202,66 +156,9 @@ public class MapManager : MonoBehaviour {
 
     #endregion
 
-    void SelectDifficulty(int difficulty) {
-        _currentDifficulty = difficulty;
-
-        for (int i = 0; i < listOfDificultySpritesActive.Count; i++) {
-            if (i <= difficulty) {
-                ListOfDifficultyImages[i].sprite = listOfDificultySpritesActive[i];
-            }
-            else {
-                ListOfDifficultyImages[i].sprite = listOfDificultySpritesDesactive[i];
-            }
-        }
-    }
-
-    void TurnBossDiffucltyScreenOn(BossDescription description) {
-
-        // Limpando objetos antigos
-        CloseMapButton.gameObject.SetActive(false);
-
-        // Mudando as informações da tela de dificuldade do boss
-        BossImage.sprite = description.BossSprite;
-        IsleName.AssetReference = description.IsleName;
-        BossDescription.text = description.Description.GetLocalizedString();
-        SecondMap.SetActive(true);
-
-        // Setando o botão de Sail
-        SailButton.onClick.RemoveAllListeners();
-        SailButton.onClick.AddListener(() => Sail(description));
-        SailButton.gameObject.SetActive(true);
-
-        // Setando as dificuldades disponiveis
-        int amountOfPhasesUnlocked = WhiteBoard.Instance.ReturnListOfUnlockedPhasesByBoss()[description.Boss];
-
-        for (int i = 0; i < ListOfDifficultyLocks.Count; i++) {
-            if (i <= amountOfPhasesUnlocked) {
-                ListOfDifficultyLocks[i].gameObject.SetActive(false);
-                ListOfDifficultyButtons[i].interactable = true;
-            }
-            else {
-                ListOfDifficultyLocks[i].gameObject.SetActive(true);
-                ListOfDifficultyButtons[i].interactable = false;
-            }
-        }
-
-        SelectDifficulty(0);
-
-    }
 
 
     #region Button Methods
-    void Sail(BossDescription description) {
-        LoadingScreenManager.CurrentLoadingScreenInfo = description.LoadingScreen[_currentDifficulty];
-        OnCloseMap?.Invoke();
-        SceneManager.LoadScene(1);
-    }
-    public void EnterSailButton(Image sailImage) {
-        sailImage.sprite = enterSailButtonSprite;
-    }
-    public void ExitSailButton(Image sailImage) {
-        sailImage.sprite = normalSailButtonSprite;
-    }
     public void EnterExitButton(Image exitImage) {
         exitImage.sprite = enterExitButtonSprite;
     }
