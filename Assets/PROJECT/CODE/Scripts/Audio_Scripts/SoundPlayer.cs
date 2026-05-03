1a6d075fa67530d1d977190f9126be69a6f0a099
Thiago Ferradaes <tferradaes@gmail.com>
2026-05-02 21:03:36 -0300

    Sounds
    Sounds to the UI added;

--- Assets/PROJECT/CODE/Scripts/Audio_Scripts/SoundPlayer.cs
+++ Assets/PROJECT/CODE/Scripts/Audio_Scripts/SoundPlayer.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour
 {
     [SerializeField] List<AK.Wwise.Event> listOfSounds;
+    [SerializeField] List<AK.Wwise.Switch> listOfSwitchs;
 
     public void PlaySound(int soundIndex) => listOfSounds[soundIndex].Post(gameObject);
+
+    public void SetSwitch(int switchIndex) => listOfSwitchs[switchIndex].SetValue(gameObject);
 }
